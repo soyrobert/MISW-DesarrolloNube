d80ffde1c249fbe71df4270771c00dc28a59110f
DaGamez <da.gamez96@uniandes.edu.co>
2024-04-09 20:46:38 -0500

    segunda version archivo rpocesar video worker video

--- src/worker/worker_video/procesar_video.py
+++ src/worker/worker_video/procesar_video.py
@@ -10,6 +10,14 @@ if not os.path.isfile(ruta_video_prueba):
 else:
     print(f"El video de prueba existe en la ruta especificada: {ruta_video_prueba}")
 
-clip=VideoFileClip(ruta_video_prueba).rotate(90)
+clip=VideoFileClip(ruta_video_prueba)
+
+# Calculate the width and height for the new aspect ratio of 16:9
+new_width = clip.w
+new_height = int(new_width * 9 / 16)
+
+# Resize the video clip to the new aspect ratio
+resized_clip = clip.resize((new_width, new_height))
+
 #save the clip
 clip.write_videofile('resources/tests/videos/video_test_rotado.mp4')
