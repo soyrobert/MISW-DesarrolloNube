58992a469a527020c9c1983c54cbc19b4bc329c9
mar-alas <m.alas@uniandes.edu.co>
2024-04-10 22:33:49 -0700

    Actualizando la seccion de la BD del docker compose, dandole permisos de ejecucion a init.sql

--- src/backend/config.py
+++ src/backend/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'clave_secreta')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://admin:admin@localhost/idlr_db')
+    SQLALCHEMY_DATABASE_URI = "postgresql://admin:admin@db/idlr_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'tu_jwt_clave_secreta')
     UPLOAD_FOLDER = '../uploads/videos_sin_editar'
