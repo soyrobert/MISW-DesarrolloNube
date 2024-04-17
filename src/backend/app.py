5a837332f1d7433f0bf850b3eb03cfbd56c79421
Jhon Freddy Puentes <jhonpuentes@192.168.10.5>
2024-04-16 22:10:46 -0500

    fix: Cambio de puerto de 8000 a 80 para gcloud

--- src/backend/app.py
+++ src/backend/app.py
@@ -19,4 +19,4 @@ def create_app(config_class=Config):
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=False)
