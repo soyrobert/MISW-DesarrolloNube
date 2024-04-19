8f4c3e4f32201656809b06b1ff3b1836af616eb9
DaGamez <da.gamez96@uniandes.edu.co>
2024-04-19 14:39:34 -0500

    ajuste variable de entorno worker video

--- src/worker/worker_video/conexion_bd.py
+++ src/worker/worker_video/conexion_bd.py
@@ -1,8 +1,11 @@
 #script ejemplo que se puede correr dentro del docker de este contendedor
 # para ejecutar una consulta sql en la base postgres
 from sqlalchemy import create_engine
+import os
 
-engine = create_engine('postgresql://admin:admin@db:5432/idlr_db')
+database_ip = os.environ['DATABASE_IP']
+
+engine = create_engine('postgresql://admin:admin@{database_ip}:5432/idlr_db')
 conn = engine.connect()
 
 query = "UPDATE tasks SET status='processed' WHERE id=1"
