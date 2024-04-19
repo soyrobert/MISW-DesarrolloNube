6bc0ef6192476272abc659bd7fc0be6b4902ca59
mar-alas <m.alas@uniandes.edu.co>
2024-04-19 09:12:33 -0700

    Configurar el backend para que use la IP de la BD si esta configurada con la variable DB_IP

--- src/backend/config.py
+++ src/backend/config.py
@@ -4,6 +4,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'clave_secreta')
     SQLALCHEMY_DATABASE_URI = "postgresql://admin:admin@db/idlr_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    if 'DB_IP' in os.environ:
+        SQLALCHEMY_DATABASE_URI = "postgresql://admin:admin@{os.environ['DB_IP']}/idlr_db"
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'tu_jwt_clave_secreta')
     UPLOAD_FOLDER = 'uploads/videos_sin_editar'
     UPLOAD_FOLDER_EDIT = 'uploads/videos_editados'
