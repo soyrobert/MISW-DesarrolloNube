4e7fdb2c6e740a16a44c3fdedd4687e24560dcff
DaGamez <da.gamez96@uniandes.edu.co>
2024-04-19 19:17:33 -0500

    ajuste conexiones backend a bd y a broker

--- src/backend/config.py
+++ src/backend/config.py
@@ -2,7 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'clave_secreta')
-    SQLALCHEMY_DATABASE_URI = "postgresql://admin:admin@db/idlr_db"
+    DATABASE_IP= os.environ['DATABASE_IP']
+    SQLALCHEMY_DATABASE_URI = "postgresql://admin:admin@" + DATABASE_IP + "/idlr_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     if 'DB_IP' in os.environ:
         SQLALCHEMY_DATABASE_URI = "postgresql://admin:admin@{os.environ['DB_IP']}/idlr_db"
