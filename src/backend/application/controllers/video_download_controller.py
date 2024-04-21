e1916ff2207389a60e6beeb9f7d72269b12f1f8a
Jhon Freddy Puentes <jhonpuentes@192.168.10.8>
2024-04-21 15:31:29 -0500

    fix(video): Ajuste para obtener video desde la ruta con ano, mes y dia.

--- src/backend/application/controllers/video_download_controller.py
+++ src/backend/application/controllers/video_download_controller.py
@@ -60,13 +60,12 @@ def download_video(ano, mes, dia, video_name):
         if video_name.endswith(".mp4"):
             video_name = video_name[:-4]
 
-
         filename = '/usr/src/app/uploads/videos_editados/'+str(ano)+'/'+str(mes)+'/'+str(dia)+'/'+str(video_name)+'.mp4'
 
         try:
             with open(filename, 'r') as file:
                 pass
-        except FileNotFoundError:
+        except FileNotFoundError as e:
             return jsonify({
                 'message': 'El video solicitado no se encuentra en el servidor. Por favor, verifique el nombre de su video e intente nuevamente.',
                 'error': str(e)
