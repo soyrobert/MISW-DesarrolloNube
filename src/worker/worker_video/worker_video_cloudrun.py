66a17090aec59cb284a3031f2faf605c8003082a
DaGamez <da.gamez96@uniandes.edu.co>
2024-05-18 18:23:19 -0500

    se agrega posibilidad de log

--- src/worker/worker_video/worker_video_cloudrun.py
+++ src/worker/worker_video/worker_video_cloudrun.py
@@ -1,8 +1,12 @@
 import os
+import logging
 
 from flask import Flask,request,jsonify
 
 app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 @app.route("/",methods=['POST'])
 def handle_post():
@@ -12,8 +16,10 @@ def handle_post():
             "message": "Received data successfully",
             "data": data
         }
+        app.logger.info("mensaje recibido exitosamente, data: " + str(data))
         return jsonify(response), 200
     else:
+        app.logger.error("error en la solicitud")
         return jsonify({"error": "Request must be JSON"}), 400
 
 if __name__ == "__main__":
